85b9ed8d44841821ba15e71f196785ffd9fe4045
Alasdia <alasndia04@gmail.com>
2026-05-16 13:45:14 +0000

    Correction chargement dotenv et emails Resend

--- backend/services/email_service.py
+++ backend/services/email_service.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 resend.api_key = os.getenv("RESEND_API_KEY")
-print("API KEY =", os.getenv("RESEND_API_KEY"))
 
 def send_payment_email(to_email, pdf_path):
 
@@ -15,14 +14,13 @@ def send_payment_email(to_email, pdf_path):
 
     pdf_base64 = base64.b64encode(pdf_bytes).decode("utf-8")
 
+    print(pdf_base64[:50])
+
     resend.Emails.send({
-        "from": "Xepay <noreply@xepay.com>",
+        "from": "Xepay <onboarding@resend.dev>",
         "to": [to_email],
         "subject": "Paiement reçu",
-        "html": """
-            <h2>Paiement confirmé ✅</h2>
-            <p>Votre facture PDF est jointe.</p>
-        """,
+        "html": "<h1>Facture PDF</h1>",
         "attachments": [
             {
                 "filename": "facture.pdf",
