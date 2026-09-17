0432ad0abdbb8a558ce128be5d23db57d77c6e78
Alasdia <alasndia04@gmail.com>
2026-09-17 03:48:10 +0000

    contenu chargé dynamiquement

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -81,7 +81,7 @@ function handleFreeLimit(data) {
         document.getElementById("createLinkModal")
       )
       createModal.hide()
-      const upgradeModal = bootstrap.Modal(
+      const upgradeModal = bootstrap.getOrCreateInstance(
         document.getElementById("modalUpgrade")
       )
       upgradeModal.show()
@@ -218,7 +218,7 @@ async function createLink() {
   if (!res.ok) {
     console.log("DATA:", data)
     if (data.detail && data.detail.upgrade === true) {
-      const modal = bootstrap.Modal(document.getElementById("modalUpgrade"))
+      const modal = bootstrap.getOrCreateInstance(document.getElementById("modalUpgrade"))
       modal.show()
       return
     }
@@ -328,7 +328,7 @@ const createBtn = document.getElementById("createBtn")
   withdrawBtn.addEventListener("click", (e) => {
     e.preventDefault()
     if (window.user === "free") {
-      const upgradeModal = bootstrap.Modal(
+      const upgradeModal = bootstrap.getOrCreateInstance(
         document.getElementById("modalUpgrade")
       )
       upgradeModal.show()
@@ -345,7 +345,7 @@ document.querySelectorAll(".upgrade-btn").forEach(btn => {
 function showUpgradeModal(feature = null) {
   const plan = localStorage.getItem("plan");
   updateUpgradeModal(plan, feature);
-  const modal = bootstrap.Modal(
+  const modal = bootstrap.getOrCreateInstance(
     document.getElementById("modalUpgrade")
   );
   modal.show();
